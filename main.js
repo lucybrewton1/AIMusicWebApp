5120518bdf32d7de8e8aa9d451d6c79e6cc5da1c
lucybrewton1 <76742499+lucybrewton1@users.noreply.github.com>
2022-05-12 07:39:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+song = "";
+song2 = "";
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+function preload() {
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+function setup() {
+    canvas = createCanvas(600,500);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotResults);
+}
+function modelLoaded() {
+    console.log("model is loaded");
+}
+function gotResults(results) {
+    if (results.length>0) {
+        console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = "+rightWristX+" Right Wrist Y = "+rightWristY);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = "+leftWristX+" Left Wrist Y = "+leftWristY);
+    }
+}
+function draw() {
+    image(video, 0, 0, 600, 500);
+    canvas.position(570,280);
+}
